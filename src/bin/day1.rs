e30fc628c5a0ce64411946cf11c525d9723b4252
Alex Whitney <adwhit@fastmail.com>
2019-12-01 22:37:01 +0000

    day 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,31 @@
+use std::fs;
+
+fn read() -> Vec<i64> {
+    fs::read_to_string("data/01.1")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect()
+}
+
+fn payload(base: i64) -> i64 {
+    let extra = base / 3 - 2;
+    if extra <= 0 {
+        base
+    } else {
+        base + payload(extra)
+    }
+}
+
+
+fn main() {
+    let vals = read();
+    let res1: i64 = vals.iter().map(|&val| payload(val) - val).sum();
+    println!("{}", res1);
+}
+
+
+#[test]
+fn test_payload() {
+    assert_eq!(payload(1969) - 1969, 966);
+}
