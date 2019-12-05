4b42ce4c9322363b5abb6965ef72ee8baef4e775
Alex Whitney <adwhit@fastmail.com>
2019-12-05 19:12:27 +0000

    refactor day02

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,3 +1,5 @@
+use advent2019::Machine;
+
 fn read() -> Vec<usize> {
     std::fs::read_to_string("data/02")
         .unwrap()
@@ -7,51 +9,19 @@ fn read() -> Vec<usize> {
         .collect()
 }
 
-fn run(data: &mut Vec<usize>) -> usize {
-    let mut posn = 0;
-
-    fn getvals(data: &[usize], posn: usize) -> (usize, usize, usize) {
-        let v1 = data[data[posn + 1]];
-        let v2 = data[data[posn + 2]];
-        let target = data[posn + 3];
-        (v1, v2, target)
-    }
-
-    loop {
-        match data[posn] {
-            1 => {
-                let (v1, v2, target) = getvals(&data, posn);
-                data[target] = v1 + v2;
-            }
-            2 => {
-                let (v1, v2, target) = getvals(&data, posn);
-                data[target] = v1 * v2;
-            }
-            99 => return data[0],
-            other => panic!("Bad opcode at posn {}: {}", posn, other),
-        }
-        posn += 4;
-    }
-}
-
 fn main() {
-    let mut data = read();
-    data[1] = 12;
-    data[2] = 2;
+    let data = read();
+    let mut machine = Machine::init(data.clone(), 12, 2);
 
-    {
-        let mut data = data.clone();
-        let out = run(&mut data);
-        println!("{}", out);
-    }
+    let res = machine.run();
+
+    println!("{}", res);
 
     for noun in 0..=99 {
         for verb in 0..=99 {
-            let mut data = data.clone();
-            data[1] = noun;
-            data[2] = verb;
-            run(&mut data);
-            if data[0] == 19690720 {
+            let mut machine = Machine::init(data.clone(), noun, verb);
+            let res = machine.run();
+            if res == 19690720 {
                 println!("noun {} verb {} outcome {}", noun, verb, 100 * noun + verb);
                 return;
             }
@@ -61,7 +31,8 @@ fn main() {
 
 #[test]
 fn example() {
-    let mut data = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
-    run(&mut data);
-    assert_eq!(data, vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
+    let data = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+    let mut machine = Machine::new(data);
+    machine.run();
+    assert_eq!(machine.state(), &[3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,67 @@
+pub struct Machine {
+    data: Vec<usize>,
+    ip: usize
+}
 
+impl Machine {
+    pub fn new(data: Vec<usize>) -> Self {
+        Self {
+            data,
+            ip: 0
+        }
+    }
+
+    pub fn init(mut data: Vec<usize>, v1: usize, v2: usize) -> Self {
+        data[1] = v1;
+        data[2] = v2;
+        Self {
+            data,
+            ip: 0
+        }
+    }
+
+    pub fn state(&self) -> &[usize] {
+        &self.data
+    }
+
+    fn fetch(&mut self) -> usize {
+        let rtn = self.data[self.ip];
+        self.ip += 1;
+        rtn
+    }
+
+    fn fetch_deref(&mut self) -> usize {
+        let ptr = self.data[self.ip];
+        self.ip += 1;
+        self.data[ptr]
+    }
+
+    fn set(&mut self, pos: usize, val: usize) {
+        self.data[pos] = val
+    }
+
+    fn get(&self, posn: usize) -> usize {
+        self.data[posn]
+    }
+
+    pub fn run(&mut self) -> usize {
+        loop {
+            match self.fetch() {
+                1 => {
+                    let v1 = self.fetch_deref();
+                    let v2 = self.fetch_deref();
+                    let target = self.fetch();
+                    self.set(target, v1 + v2)
+                }
+                2 => {
+                    let v1 = self.fetch_deref();
+                    let v2 = self.fetch_deref();
+                    let target = self.fetch();
+                    self.set(target, v1 * v2)
+                }
+                99 => return self.get(0),
+                other => panic!("Bad opcode at posn {}: {}", self.ip - 1, other),
+            }
+        }
+    }
+}
